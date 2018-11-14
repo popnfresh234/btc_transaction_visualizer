ed55a4805272b794006f1f4c66947eb213c2f6e3
Alex <ajhollid@gmail.com>
2018-11-14 11:24:02 -0800

    Rename stlye to css for consistency across projects, add skeleton

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,9 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new CopyWebpackPlugin([
       { from: './src/index.html', to: './index.html' },
-      { from: './src/styles/normalize.css', to: './normailze.css' },
-      { from: './src/styles/style.css', to: './style.css' },
+      { from: './src/css/skeleton.css', to: './skeleton.css' },
+      { from: './src/css/normalize.css', to: './normailze.css' },
+      { from: './src/css/style.css', to: './style.css' },
       { from: './src/favicon.ico', to: './favicon.ico' },
     ]),
   ],
